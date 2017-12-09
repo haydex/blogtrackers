a5ba10997f3def003d214e53b69c53953375a5ce
Haydex <hayddex@gmail.com>
2017-12-08 22:15:20 -0600

    Update 2

--- app/src/main/java/edu/ualr/cpsc5367/hkalrubaye/blogtrackers/BlogDetailActivity.java
+++ app/src/main/java/edu/ualr/cpsc5367/hkalrubaye/blogtrackers/BlogDetailActivity.java
@@ -17,7 +17,7 @@ public class BlogDetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_item_detail);
+        setContentView(R.layout.activity_blog_detail);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
         setSupportActionBar(toolbar);
